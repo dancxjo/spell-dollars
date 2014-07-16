ba6fb7a876aa9a0496cbfd48b6a90ef89a09d265
dancxjo <tdreed@gmail.com>
2014-07-16 08:34:10 -0700

    Create main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "Spellable.h"
+
+using namespace std;
+
+int main()
+{
+  int i = 195;
+  long long l = 9223372036854775807;
+  float f = 89759.12312;
+  
+  Spellable<int> si(&i);
+  Spellable<long long> sl(&l);
+  Spellable<float> sf(&f);
+  
+  cout << si.spell() << endl;
+  cout << sl.spell() << endl;
+  cout << sf.spell() << endl;
+
+  return 0;
+}
