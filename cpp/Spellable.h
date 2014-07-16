0113105a25be137da57210a3ff077d2709203cb9
dancxjo <tdreed@gmail.com>
2014-07-16 10:53:26 -0700

    Fiddling with linking issues

--- cpp/Spellable.h
+++ cpp/Spellable.h
@@ -5,12 +5,18 @@
 #include <string>
 
 template <class T>
-
 class Spellable {
 	T* value;
 public:
+	Spellable() {};
 	Spellable(T* value): value(value) {};
+	void SetValue(T* v) { value = v; }
 	std::string spell();
 };
+
+// http://www.codeproject.com/Articles/48575/How-to-define-a-template-class-in-a-h-file-and-imp
+// The linker gets confused if we don't include the implementation here.
+#include "Spellable.cpp"
+
 
 #endif // Spellable.h
