24920335aac42ac11eb2ee69662b89a8850a93a2
dancxjo <tdreed@gmail.com>
2014-07-16 10:52:37 -0700

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,19 +2,20 @@
 
 #include "Spellable.h"
 
-int main()
-{
-  int i = 195;
-  long long l = 9223372036854775807;
-  float f = 89759.12312;
-  
-  Spellable<int> si(&i);
-  Spellable<long long> sl(&l);
-  Spellable<float> sf(&f);
-  
-  cout << si.spell() << endl;
-  cout << sl.spell() << endl;
-  cout << sf.spell() << endl;
+using namespace std;
 
-  return 0;
+int main(int argc, char** argv) {
+	int i = 195;
+	long long l = 9223372036854775807;
+	float f = 89759.12312;
+  
+	Spellable<int> si(&i);
+	Spellable<long long> sl(&l);
+	Spellable<float> sf(&f);
+  
+	cout << si.spell() << endl;
+	cout << sl.spell() << endl;
+	cout << sf.spell() << endl;
+
+	return 0;
 }
