6c4f3298365f8c4a52b7412de5182d9f7ffeb920
dancxjo <tdreed@gmail.com>
2014-07-16 08:36:30 -0700

    Create Spellable.h

---
+++ cpp/Spellable.h
@@ -0,0 +1,16 @@
+// Spellable.h
+#ifndef SPELLABLE_H
+#define SPELLABLE_H
+
+#include <string>
+
+template <class T>
+
+class Spellable {
+    T* value;
+public:
+	Spellable(T* value): value(value) {};
+	std::string spell();
+};
+
+#endif // Spellable.h
