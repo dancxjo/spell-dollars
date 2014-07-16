9ea8b7552bac8c266e1b304965525bdc0497d4d7
dancxjo <tdreed@gmail.com>
2014-07-16 13:35:56 -0700

    Overload equality tests

--- cpp/Spellable.h
+++ cpp/Spellable.h
@@ -9,9 +9,33 @@ class Spellable {
 	T* value;
 public:
 	Spellable() {};
-	Spellable(T* value): value(value) {};
+	Spellable(T* value): value(value) {}
+	Spellable(T v) { value = new T(v); }
+	
 	void SetValue(T* v) { value = v; }
+	void SetValue(T v) { value = new T(v); }
+
+	T GetValue() const { return *value; }
+	
 	std::string spell();
+	
+	// Let's do arithmetic!
+	Spellable & operator=(const T &rhs) {
+		SetValue(rhs);
+	}
+	
+	Spellable & operator+=(const T &rhs) {
+		*value += rhs;
+		return *this;
+	}
+	
+	bool operator==(const T &other) const {
+		return *value == other;
+	}
+	
+	bool operator==(const std::string &other) const {
+		return spell() == other;
+	}
 };
 
 // http://www.codeproject.com/Articles/48575/How-to-define-a-template-class-in-a-h-file-and-imp
